d52cd522ba7d210f3239b44d26e4c475e1c20d6e
Shakena <shakenacme@gmail.com>
2021-07-29 16:31:22 -0400

    Mobile nav: Moved mobileNav styles to open class; Added display none to mobile nav outside of open class

--- app/js/script.js
+++ app/js/script.js
@@ -1,17 +1,20 @@
 /* Mobile Menu */
 const btnHamburger = document.querySelector('#btnHamburger');
+const header = document.querySelector('.header');
 
 // Mobile Menu: click even for hamburger
 btnHamburger.addEventListener('click', function(){
 
-   // if btnHamburger contains open
-   if (btnHamburger.classList.contains('open')) {
+   // if header contains open
+   if (header.classList.contains('open')) {
       //remove open
-      btnHamburger.classList.remove('open');
+      header.classList.remove('open');
    }
    else {
       //else add open
-      btnHamburger.classList.add('open');
+      header.classList.add('open');
    }
 
 })
+
+//adjust js so we are adding the header class in the header element
