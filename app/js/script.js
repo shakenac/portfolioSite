273607fbe04f6cfff0c70c2255b8554bf7bfc0bd
Shakena <shakenacme@gmail.com>
2021-07-30 14:18:00 -0400

    Added overflow hidden to body tag when mobile menu is open

--- app/js/script.js
+++ app/js/script.js
@@ -2,6 +2,8 @@
 const btnHamburger = document.querySelector('#btnHamburger');
 const header = document.querySelector('.header');
 const mobileNav = document.querySelector('.header__mobileNav');
+const bodyTag = document.querySelector("body");
+
 
 /// Mobile Menu: click even for hamburger
 btnHamburger.addEventListener('click', function(){
@@ -13,11 +15,13 @@ btnHamburger.addEventListener('click', function(){
       header.classList.remove('open');
       mobileNav.classList.remove('fade-in');
       mobileNav.classList.add('fade-out');
+      bodyTag.style.overflow = "visible";
    }
    else {
       //else add open -- open the hamburger menu
       header.classList.add('open');
       mobileNav.classList.remove('fade-out');
       mobileNav.classList.add('fade-in');
+      bodyTag.style.overflow = "hidden";
    }
 })
