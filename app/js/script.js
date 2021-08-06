8a079f1162e866282bee369b6b8bc9d694176460
Shakena <shakenacme@gmail.com>
2021-08-06 12:29:18 -0400

    Added to workLinks function to get data-type2 attributes value and add/remove show and hide classes to items

--- app/js/script.js
+++ app/js/script.js
@@ -48,16 +48,25 @@ workLinks.onclick = function(selectedLink) {
       //getting data-type value of selected item and storing in a filtername variable (data-* is considered an attribute)
       let filterType = selectedLink.target.getAttribute('data-type');
 
+      //variable: getting data-type2 value of selected item and storing in a filtername variable
+      let filterType2 = selectedLink.target.getAttribute('data-type2');
+
       //getting image data-name value
-      //getting image data-type value
          //looping through each (forEach) array item (filterItem) and adding the function to each item
       filterItem.forEach(function(item) {
 
          let filterItemType = item.getAttribute('data-type');
 
+         //variable: items data-type2 value
+         let filterItemType2 = item.getAttribute('data-type2');
+
          //if user selected item data-type value is eqaul to workLinks data-type
             // or user selected item data-type value is equal to 'all'
          if ((filterItemType == filterType) ||  filterType == 'all') {
+            item.classList.add('show');
+            item.classList.remove('hide');
+         }
+         else if ((filterItemType2 == filterType) || filterType == 'all') {
             item.classList.add('show');
             item.classList.remove('hide');
          }
@@ -66,8 +75,13 @@ workLinks.onclick = function(selectedLink) {
             item.classList.remove('show');
          }
       })
-
-
    }
-
 }
+
+
+
+/* Need to add additional data attribute to HHCS and DMK items. Currently they have data-type for "graphicDesign", they also need data for 'website' */
+
+//1. Add new data* attribute (with different name such as "data-type2")
+//2. Get data-type2 value
+//if data-type2 value is
