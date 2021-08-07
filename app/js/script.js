9d8be07fc779272097ee6b6e4be414361c24b75b
Shakena <shakenacme@gmail.com>
2021-08-06 23:39:31 -0400

    moved worklinks event listener into if statement to prevent error message on other html pages

--- app/js/script.js
+++ app/js/script.js
@@ -37,7 +37,6 @@ mobileNav.addEventListener('click', function(selectedLink) {
 })
 
 
-
 //// Filtering Portfolio Items
 
 // Selecting all required elements
@@ -46,49 +45,55 @@ const workLinks = document.querySelector('.work__links');
  //portfolio items
 const filterItem = document.querySelectorAll(".item");
 
-workLinks.onclick = function(selectedLink) {
-   //when user clicks on work__links ul
-   if (selectedLink.target.classList.contains('filter-link')) {
+//once window load
+window.onload = function() {
+   if (workLinks) {
+      
+      workLinks.addEventListener('click', function(selectedLink) {
+         //when user clicks on work__links ul
+         if (selectedLink.target.classList.contains('filter-link')) {
 
-      // remove the work__current class
-      workLinks.querySelector('.work__current').classList.remove('work__current');
-      //add work__current class to selectedLink (whatever user clicked on)
-      selectedLink.target.classList.add('work__current');
+            // remove the work__current class
+            workLinks.querySelector('.work__current').classList.remove('work__current');
+            //add work__current class to selectedLink (whatever user clicked on)
+            selectedLink.target.classList.add('work__current');
 
-      //getting data-type value of selected item and storing in a filtername variable (data-* is considered an attribute)
-      let filterType = selectedLink.target.getAttribute('data-type');
+            //getting data-type value of selected item and storing in a filtername variable (data-* is considered an attribute)
+            let filterType = selectedLink.target.getAttribute('data-type');
 
-      //variable: getting data-type2 value of selected item and storing in a filtername variable
-      let filterType2 = selectedLink.target.getAttribute('data-type2');
+            //variable: getting data-type2 value of selected item and storing in a filtername variable
+            let filterType2 = selectedLink.target.getAttribute('data-type2');
 
-      //getting image data-name value
-         //looping through each (forEach) array item (filterItem) and adding the function to each item
-      filterItem.forEach(function(item) {
+            //getting image data-name value
+               //looping through each (forEach) array item (filterItem) and adding the function to each item
+            filterItem.forEach(function(item) {
 
-         let filterItemType = item.getAttribute('data-type');
+               let filterItemType = item.getAttribute('data-type');
 
-         //variable: items data-type2 value
-         let filterItemType2 = item.getAttribute('data-type2');
+               //variable: items data-type2 value
+               let filterItemType2 = item.getAttribute('data-type2');
 
-         //if user selected item data-type value is eqaul to workLinks data-type
-            // or user selected item data-type value is equal to 'all'
-         if ((filterItemType == filterType) ||  filterType == 'all') {
-            item.classList.add('show');
-            item.classList.add('scale-in');
-            item.classList.remove('hide');
-            // item.classList.remove('scale-out');
-         }
-         else if ((filterItemType2 == filterType) || filterType == 'all') {
-            item.classList.add('show');
-            item.classList.add('scale-in');
-            item.classList.remove('hide');
-            // item.classList.remove('scale-out');
-         }
-         else {
-            item.classList.add('hide');
-            item.classList.add('scale-out');
-            item.classList.remove('show');
-            // item.classList.remove('scale-in');
+               //if user selected item data-type value is eqaul to workLinks data-type
+                  // or user selected item data-type value is equal to 'all'
+               if ((filterItemType == filterType) ||  filterType == 'all') {
+                  item.classList.add('show');
+                  item.classList.add('scale-in');
+                  item.classList.remove('hide');
+                  // item.classList.remove('scale-out');
+               }
+               else if ((filterItemType2 == filterType) || filterType == 'all') {
+                  item.classList.add('show');
+                  item.classList.add('scale-in');
+                  item.classList.remove('hide');
+                  // item.classList.remove('scale-out');
+               }
+               else {
+                  item.classList.add('hide');
+                  item.classList.add('scale-out');
+                  item.classList.remove('show');
+                  // item.classList.remove('scale-in');
+               }
+            })
          }
       })
    }
