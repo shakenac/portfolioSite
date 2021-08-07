dd2deb4b01597f613a306e810e7bb63920cb02b1
Shakena <shakenacme@gmail.com>
2021-08-06 23:59:15 -0400

    Removed .show class from work.scss and javascript (its not needed)

--- app/js/script.js
+++ app/js/script.js
@@ -1,54 +1,55 @@
-/* Mobile Menu */
-const btnHamburger = document.querySelector('#btnHamburger');
-const header = document.querySelector('.header');
-const mobileNav = document.querySelector('.header__mobileNav');
-const bodyTag = document.querySelector("body");
-
-
-/// Mobile Menu: click even for hamburger
-btnHamburger.addEventListener('click', function(){
-
-   // if header contains open
-   if (header.classList.contains('open')) {
-
-      //remove open -- close hamburger menu
-      header.classList.remove('open');
-      mobileNav.classList.remove('fade-in');
-      mobileNav.classList.add('fade-out');
-      bodyTag.style.overflow = "visible";
-   }
-   else {
-      //else add open -- open the hamburger menu
-      header.classList.add('open');
-      mobileNav.classList.remove('fade-out');
-      mobileNav.classList.add('fade-in');
-      bodyTag.style.overflow = "hidden";
-   }
-})
-
-// when link in mobile nav is clicked, hide mobile nav
-mobileNav.addEventListener('click', function(selectedLink) {
-   if (selectedLink.target.tagName.toLowerCase() == 'a') {
-      header.classList.remove('open');
-      mobileNav.classList.remove('fade-in');
-      mobileNav.classList.add('fade-out');
-      bodyTag.style.overflow = "visible";
-   }
-})
-
-
-//// Filtering Portfolio Items
-
-// Selecting all required elements
-//work links ul
-const workLinks = document.querySelector('.work__links');
- //portfolio items
-const filterItem = document.querySelectorAll(".item");
-
 //once window load
 window.onload = function() {
+   /* Mobile Menu */
+   const btnHamburger = document.querySelector('#btnHamburger');
+   const header = document.querySelector('.header');
+   const mobileNav = document.querySelector('.header__mobileNav');
+   const bodyTag = document.querySelector("body");
+
+
+   /// Mobile Menu: click even for hamburger
+   btnHamburger.addEventListener('click', function(){
+
+      // if header contains open
+      if (header.classList.contains('open')) {
+
+         //remove open -- close hamburger menu
+         header.classList.remove('open');
+         mobileNav.classList.remove('fade-in');
+         mobileNav.classList.add('fade-out');
+         bodyTag.style.overflow = "visible";
+      }
+      else {
+         //else add open -- open the hamburger menu
+         header.classList.add('open');
+         mobileNav.classList.remove('fade-out');
+         mobileNav.classList.add('fade-in');
+         bodyTag.style.overflow = "hidden";
+      }
+   })
+
+   // when link in mobile nav is clicked, hide mobile nav
+   mobileNav.addEventListener('click', function(selectedLink) {
+      if (selectedLink.target.tagName.toLowerCase() == 'a') {
+         header.classList.remove('open');
+         mobileNav.classList.remove('fade-in');
+         mobileNav.classList.add('fade-out');
+         bodyTag.style.overflow = "visible";
+      }
+   })
+
+
+   //// Filtering Portfolio Items
+
+   // Selecting all required elements
+   //work links ul
+   const workLinks = document.querySelector('.work__links');
+    //portfolio items
+   const filterItem = document.querySelectorAll(".item");
+
+
    if (workLinks) {
-      
+
       workLinks.addEventListener('click', function(selectedLink) {
          //when user clicks on work__links ul
          if (selectedLink.target.classList.contains('filter-link')) {
@@ -76,13 +77,13 @@ window.onload = function() {
                //if user selected item data-type value is eqaul to workLinks data-type
                   // or user selected item data-type value is equal to 'all'
                if ((filterItemType == filterType) ||  filterType == 'all') {
-                  item.classList.add('show');
+                  // item.classList.add('show');
                   item.classList.add('scale-in');
                   item.classList.remove('hide');
                   // item.classList.remove('scale-out');
                }
                else if ((filterItemType2 == filterType) || filterType == 'all') {
-                  item.classList.add('show');
+                  // item.classList.add('show');
                   item.classList.add('scale-in');
                   item.classList.remove('hide');
                   // item.classList.remove('scale-out');
@@ -90,7 +91,7 @@ window.onload = function() {
                else {
                   item.classList.add('hide');
                   item.classList.add('scale-out');
-                  item.classList.remove('show');
+                  // item.classList.remove('show');
                   // item.classList.remove('scale-in');
                }
             })
